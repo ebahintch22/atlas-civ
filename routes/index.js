8f9e040038f1bf9faf48112c5ba0d2c53b9a4fe1
ebahintch <bahintchie.eric@yahoo.fr>
2019-08-27 12:51:25 +0000

    Update the package.json file var express = require('express'); var router = express.Router(); var path = require('path');
    /* GET home page. */
    router.get('/', function(req, res, next) {
    
      var options = {
      	root: path.join(__dirname, '../public')
      }
      res.sendFile( 'v7-index.html', options);
    
    });
    
    module.exports = router;

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var options = {
+  	root: path.join(__dirname, '../public')
+  }
+  res.sendFile( 'v7-index.html', options);
+
 });
 
 module.exports = router;
