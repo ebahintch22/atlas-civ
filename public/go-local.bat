4ddc4e2637386b4b0c667a45d2c799822be699e3
ebahintch <bahintchie.eric@yahoo.fr>
2020-06-07 07:49:09 +0000

    Saturday Nigth Fever

---
+++ public/go-local.bat
@@ -0,0 +1,2 @@
+COPY /A ..\config\local\package.json  ..\package.json
+COPY /A ..\config\local\.env  ..\.env
---
+++ public/go-remote.bat
@@ -0,0 +1,2 @@
+COPY /A ..\config\remote\package.json  ..\package.json
+COPY /A ..\config\remote\.env  ..\.env
