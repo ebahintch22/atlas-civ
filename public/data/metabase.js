8abcd848a00031bfc968ef8df19fd743018f9b27
ebahintch <bahintchie.eric@yahoo.fr>
2020-08-06 08:29:17 +0000

    ASCIV-2020-AUG

---
+++ public/data/metabase.js
@@ -0,0 +1,31 @@
+//metabase.js
+	tables : [
+		"demographic",
+		"human_ressource",
+		"ratio_prestataire_pop",
+		"repartition_etabliss_sante",
+		"ratio_hrsn_espc_pop",
+
+		"other_material",
+		"tech_platform",
+		"ratio_ambulance_structure_sante",
+		"repartition_struct_transfusion",
+		"std",
+
+		"covid-19",
+		"covid-19-june16",
+		"geographic_accessibility",
+		"taux_utilisation_serv_sante",
+		"taux_util_serv_sante_par_etablissmnt",
+		
+		"taux_frequentation_service_sante",
+		"taux_frequentation",
+		"effectif_consultant",
+		"paludisme_u5_ans",
+		"paludisme_glo",
+		"incidence_IRA_u5_ans",
+		"incidence_IRA_glo",
+		"incidence_anemie",
+		"incidence_coqueluche",
+		"deces_notifie_et_causes"
+	],
--- public/js/app-manifest-data.js
+++ public/js/app-manifest-data.js
@@ -231,23 +231,96 @@ var metaDataBase = {
 			color_palette: "YlGnBu",
 			field_selected : default_field_selection,
 			data_fields : [
-				{ fld_name: "FLD1", short_name: "Population totale 2017", long_name: "Population totale 2017", data_type: "INT", unit: "habitants"},
-				{ fld_name: "FLD2", short_name: "Population 0 à 11 mois", long_name: "Population 0 à 11 mois", data_type: "INT", unit: "habitants"},
-				{ fld_name: "FLD3", short_name: "Population 0 à 4 ans", long_name: "Population 0 à 4 ans", data_type: "INT", unit: "habitants"},
-				{ fld_name: "FLD4", short_name: "Population de moins de 15 ans", long_name: "Population de moins de 15 ans", data_type: "INT", unit: "habitants"},
-				{ fld_name: "FLD5", short_name: "Population de 15 ans et plus", long_name: "Population de 15 ans et plus", data_type: "INT", unit: "habitants"},
-				{ fld_name: "FLD6", short_name: "Nb. de femmes en âge de procréer", long_name: "Femme en âge de procréer", data_type: "INT", unit: "FAP"},
-				{ fld_name: "FLD7", short_name: "Nb. de grossesses attendues", long_name: "Grossesses attendues", data_type: "INT", unit: "grossesses attendues"},
-				{ fld_name: "FLD8", short_name: "Nb. de naissances attendues", long_name: "Naissances attendues", data_type: "INT", unit: "naissances attendues"},
-				{ fld_name: "FLD9", short_name: "Nb. de Complications obstétricales attendues", long_name: "Complications obstétricales attendues", data_type: "INT", unit: "cas de complication attendus"}
+				{
+					fld_name: "FLD1",
+					short_name: "Population totale 2017",
+					long_name: "Population totale 2017",
+					data_type: "INT",
+					unit: "habitants",
+					renderer : {
+			 			"default": {
+							source: "manual",
+							threshold: [ 500000, 1000000, 1500000, 2000000, 3000000 ],
+							colormap:   ['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 500m", "500-400m", "1000-1500m" , "1500-2000m" , "2000-3000m", "3000m et plus" ],
+							legendtitle: "Population totale 2017 par Région"
+						},
+			 			"district_sante": {
+							source: "manual",
+							threshold: [ 50000, 100000, 200000, 500000, 1000000 ],
+							colormap:  ['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 50m", "50-100m", "100-200m" , "200-500m" , "500-1000m", "1000m et +" ],
+							legendtitle: "Population totale 2017 par district"
+						}						
+					}
+				},
+				{
+					fld_name: "FLD2",
+					short_name: "Population 0 à 11 mois",
+					long_name: "Population 0 à 11 mois",
+					data_type: "INT",
+					unit: "habitants"
+				},
+				{
+					fld_name: "FLD3",
+					short_name: "Population 0 à 4 ans",
+					long_name: "Population 0 à 4 ans",
+					data_type: "INT",
+					unit: "habitants"
+				},
+				{
+					fld_name: "FLD4",
+					short_name: "Population de moins de 15 ans",
+					long_name: "Population de moins de 15 ans",
+					data_type: "INT",
+					unit: "habitants"
+				},
+				{
+					fld_name: "FLD5",
+					short_name: "Population de 15 ans et plus",
+					long_name: "Population de 15 ans et plus",
+					data_type: "INT",
+					unit: "habitants"
+				},
+				{
+					fld_name: "FLD6",
+					short_name: "Nb. de femmes en âge de procréer",
+					long_name: "Femme en âge de procréer",
+					data_type: "INT",
+					unit: "FAP"
+				},
+				{
+					fld_name: "FLD7",
+					short_name: "Nb. de grossesses attendues",
+					long_name: "Grossesses attendues",
+					data_type: "INT",
+					unit: "grossesses attendues"
+				},
+				{
+					fld_name: "FLD8",
+					short_name: "Nb. de naissances attendues",
+					long_name: "Naissances attendues",
+					data_type: "INT",
+					unit: "naissances attendues"
+				},
+				{
+					fld_name: "FLD9",
+					short_name: "Nb. de Complications obstétricales attendues",
+					long_name: "Complications obstétricales attendues",
+					data_type: "INT",
+					unit: "cas de complication attendus"
+				}
 			]
+
 		},
 		{
 			index : 4,
 			valid: true,
-			name: "human_ressource",
+			name: "03_human_ressource",
 			layerList : [  "district_sante", "region_sante" ],
-			table_num :"Tableau-5",
+			table_num :"Tableau-3",
 			label: "03- Ressources des systèmes de santé - Personnel",
 			unit: "effectif",
 			article: "d'",
@@ -276,7 +349,7 @@ var metaDataBase = {
 		{
 			index : 5,
 			valid: true,
-			name : "ratio_prestataire_pop", 
+			name : "04_ratio_prestataire_pop", 
 			layerList :  [ "district_sante", "region_sante"],
 			table_num :"Tableau 6",
 			label: "04- Ratio Prestataires de soins - Population",
@@ -548,21 +621,67 @@ var metaDataBase = {
 			 		short_name: "Population totale 2017",
 			 		long_name: "Population totale 2017",
 			 		data_type: "INT",
-			 		unit: "habitants"
+			 		unit: "habitants",
+			 		renderer: {
+			 			"default": {
+							source: "manual",
+							threshold: [ 500000, 1000000, 1500000, 2000000, 3000000 ],
+							colormap:   ['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 500m", "500-400m", "1000-1500m" , "1500-2000m" , "2000-3000m", "3000m et plus" ],
+							legendtitle: "Population totale 2017 par Région"
+						},
+			 			"district_sante": {
+							source: "manual",
+							threshold: [ 50000, 100000, 200000, 500000, 1000000 ],
+							colormap:  ['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 50m", "50-100m", "100-200m" , "200-500m" , "500-1000m", "1000m et +" ],
+							legendtitle: "Population totale 2017 par district"
+						}						
+					}
 			 	},
 			 	{
 			 		fld_name: "FLD2",
 			 		short_name: "Nbre total de Consultants",
 			 		long_name: "Nombre total de Consultants",
 			 		data_type: "INT",
-			 		unit: "consultants"
+			 		unit: "consultants",
+			 		renderer: {
+			 			"default": {
+							source: "manual",
+							threshold: [ 200000, 500000, 750000, 1000000, 1500000 ],
+							colormap:   ['#f6eff7','#d0d1e6','#a6bddb','#67a9cf','#1c9099','#016c59'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 200m", "200-500m", "500-750m" , "750-1000m" , "1000-1500m", "1500m et plus" ],
+							legendtitle: "Nombre total de Consultants"
+						},
+			 			"district_sante": {
+							source: "manual",
+							threshold: [ 20000, 50000, 100000, 200000, 400000 ],
+							colormap:  ['#f6eff7','#d0d1e6','#a6bddb','#67a9cf','#1c9099','#016c59'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 20m", "20-50m" ,"50-100m",  "100-200m" , "200-400m", "400m et +" ],
+							legendtitle: "Nombre total de Consultants"
+						}						
+					}
 			 	},
 			 	{
 			 		fld_name: "FLD3",
 			 		short_name: "Taux d'utilisation(%)",
 			 		long_name: "Taux d'utilisation(%)",
 			 		data_type: "INT",
-			 		unit: "%"
+			 		unit: "%",
+			 		renderer: {
+			 			"default": {
+							source: "manual",
+							threshold:  [ 10, 20, 40, 60, 80],
+							colormap:   ['#ffffd4','#fee391','#fec44f','#fe9929','#d95f0e','#993404'],
+							linecolor: "#fff",
+							labelmap:  [ "moins de 500m", "500-400m", "1000-1500m" , "1500-2000m" , "2000-3000m", "3000m et plus" ],
+							legendtitle: "Taux d'utilisation des services de santé"
+						}					
+					}
 			 	}
 			 ]
 		},
